df7d4672a48e1ee62c295e15d9748ad518996d30
vitoriopaulo <vitoriopaulo@gmail.com>
2017-11-03 10:06:32 -0400

    Commit.
    Environment settled up after generate scaffold command.
    Migration done as well. Schema file updated.
    
    Relationship one to many created between
    users and movies: an user has many movies; a movie belongs to an user.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,27 +1,28 @@
-class MoviesController < ApplicationController
-  before_action :set_movie, only: [:show, :update, :destroy]
+class MoviesController < ProtectedController
+  before_action :set_movie, only: %i[show update destroy]
 
   # GET /movies
   def index
-    @movies = Movie.all
+    @movies = current_user.movies
 
     render json: @movies
   end
 
   # GET /movies/1
   def show
-    render json: @movie
+    render json: current_user.movies.find(params[:id])
   end
 
   # POST /movies
   def create
-    @movie = Movie.new(movie_params)
+    # binding.pry
+        @movie = current_user.movies.build(movie_params)
 
-    if @movie.save
-      render json: @movie, status: :created, location: @movie
-    else
-      render json: @movie.errors, status: :unprocessable_entity
-    end
+        if @movie.save
+          render json: @movie, status: :created
+        else
+          render json: @movie.errors, status: :unprocessable_entity
+        end
   end
 
   # PATCH/PUT /movies/1
@@ -35,17 +36,19 @@ class MoviesController < ApplicationController
 
   # DELETE /movies/1
   def destroy
+    # binding.pry
     @movie.destroy
+    head :no_content
   end
 
-  private
     # Use callbacks to share common setup or constraints between actions.
     def set_movie
-      @movie = Movie.find(params[:id])
+      @movie = current_user.movies.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
     def movie_params
       params.require(:movie).permit(:title, :sinopse, :year, :director, :actors, :poster, :trailer, :category, :status, :comment)
     end
+    private :set_movie, :movie_params
 end
